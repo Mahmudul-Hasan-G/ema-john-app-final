bb56a9d4a6082b33af8d7100d52b1567ecc123c4
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-04-13 06:53:40 +0200

    set-to-local-storage

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -7,10 +7,17 @@ const Cart = (props) => {
 
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for(const product of cart){
-    total = total + product.price;
+    if(product.quantity === 0){
+      product.quantity = 1;
+    }
+    
+    //product.quantity = product.quantity || 1;
+
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
-   
+    quantity = quantity + product.quantity;
   }
   const tax = (total * 0.07).toFixed(2);
   const grandTotal = total + totalShipping + parseFloat(tax);
@@ -18,7 +25,7 @@ const Cart = (props) => {
   return (
     <div className='cart'>
       <h2>Order Summary</h2>
-<h2>Selected Items: {cart.length}</h2>
+<h2>Selected Items: {quantity}</h2>
 <p>Total Price: {total} </p>
 <p>Total Shipping: {totalShipping}</p>
 <p>Tax: {tax}</p>

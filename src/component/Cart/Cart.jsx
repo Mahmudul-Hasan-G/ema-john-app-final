d02f0b55ad695c20643b8d9ea95a8e2e14cf2921
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-04-13 05:38:29 +0200

    cart-items-calculetion

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -6,18 +6,23 @@ const Cart = (props) => {
   const {cart} = props;
 
   let total = 0;
+  let totalShipping = 0;
   for(const product of cart){
     total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
+   
   }
+  const tax = (total * 0.07).toFixed(2);
+  const grandTotal = total + totalShipping + parseFloat(tax);
 
   return (
     <div className='cart'>
       <h2>Order Summary</h2>
 <h2>Selected Items: {cart.length}</h2>
 <p>Total Price: {total} </p>
-<p>Total Shipping: </p>
-<p>Tax: </p>
-<h6>Grand Total:</h6>
+<p>Total Shipping: {totalShipping}</p>
+<p>Tax: {tax}</p>
+<h6>Grand Total: {grandTotal}</h6>
     </div>
     
   );
